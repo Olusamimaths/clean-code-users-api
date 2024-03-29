cfec3a42763173a24fb06d719fa739025b246a64
olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2024-03-29 12:20:50 +0100

    :construction: Setup validator and exception filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { APP_FILTER } from '@nestjs/core';
+import { AllExceptionsFilter } from './exception-filters';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
+    AppService,
+  ],
 })
 export class AppModule {}
